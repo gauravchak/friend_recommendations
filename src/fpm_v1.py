0afcf3dd4125e3eafba447d46e55cddaba715a07
Gaurav Chakravorty <gchak207@gmail.com>
2024-09-29 18:46:52 -0700

    Simple mean pooling and then MLP

--- src/fpm_v1.py
+++ src/fpm_v1.py
@@ -18,25 +18,28 @@ class FriendingPredictionModelV1(nn.Module):
 
     Args:
         user_dim (int): Dimension of user embeddings
-        n_tasks (int, optional): Number of tasks. Defaults to 1.
+        n_tasks (int, optional): Number of tasks.
     """
 
     def __init__(
         self,
-        user_dim,
-        n_tasks=1,
+        user_dim: int,
+        n_tasks: int,
     ):
         super().__init__()
+        self.task_arch = nn.Sequential(
+            nn.Linear(in_features=4 * user_dim, out_features=n_tasks), nn.ReLU()
+        )
 
     def forward(
         self,
-        viewer_id,
-        viewer_friendings,
-        viewer_friending_timegap,
-        target_id,
-        target_friendings,
-        target_friending_timegap,
-    ):
+        viewer_id: torch.Tensor,
+        viewer_friendings: torch.Tensor,
+        viewer_friending_timegap: torch.Tensor,
+        target_id: torch.Tensor,
+        target_friendings: torch.Tensor,
+        target_friending_timegap: torch.Tensor,
+    ) -> torch.Tensor:
         """
         Forward pass of the Friending Prediction Model.
 
@@ -51,15 +54,31 @@ class FriendingPredictionModelV1(nn.Module):
         Returns:
             list: List of output tensors for each task
         """
+        mean_pooled_viewer_friendings = torch.mean(
+            viewer_friendings, dim=1
+        )  # mean pool viewer_friendings [B, N, D] to [B, D]
+        mean_pooled_target_friendings = torch.mean(
+            target_friendings, dim=1
+        )  # mean pool viewer_friendings [B, N, D] to [B, D]
+        # Concatenate all features
+        combined_features = torch.cat(
+            [
+                viewer_id,
+                mean_pooled_viewer_friendings,
+                target_id,
+                mean_pooled_target_friendings,
+            ],
+            dim=1,
+        )
+        task_logits = self.task_arch(combined_features)
+        return task_logits
 
 
 # Example usage
 batch_size, seq_length, user_dim = 32, 10, 128
 n_tasks = 3
 
-model = FriendingPredictionModelV1(
-    user_dim=user_dim, n_tasks=n_tasks, dcn_layers=3, num_experts=4
-)
+model = FriendingPredictionModelV1(user_dim=user_dim, n_tasks=n_tasks)
 
 # Generate dummy data
 viewer_id = torch.randn(batch_size, user_dim)
