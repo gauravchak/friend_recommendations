329ee915bdb5669cf698e0b70ed04b33dfb52ae8
Gaurav Chakravorty <gchak207@gmail.com>
2024-09-29 18:48:33 -0700

    remove default

--- src/fpm_v4.py
+++ src/fpm_v4.py
@@ -18,25 +18,25 @@ class FriendingPredictionModelV4(nn.Module):
 
     Args:
         user_dim (int): Dimension of user embeddings
-        n_tasks (int, optional): Number of tasks. Defaults to 1.
+        n_tasks (int, optional): Number of tasks.
     """
 
     def __init__(
         self,
-        user_dim,
-        n_tasks=1,
+        user_dim: int,
+        n_tasks: int,
     ):
         super().__init__()
 
     def forward(
         self,
-        viewer_id,
-        viewer_friendings,
-        viewer_friending_timegap,
-        target_id,
-        target_friendings,
-        target_friending_timegap,
-    ):
+        viewer_id: torch.Tensor,
+        viewer_friendings: torch.Tensor,
+        viewer_friending_timegap: torch.Tensor,
+        target_id: torch.Tensor,
+        target_friendings: torch.Tensor,
+        target_friending_timegap: torch.Tensor,
+    ) -> torch.Tensor:
         """
         Forward pass of the Friending Prediction Model.
 
